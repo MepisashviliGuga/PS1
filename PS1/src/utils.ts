8bd3098648925b16d945aa7951935727f8c37b2a
Guga <Guga@DESKTOP-40DOUJ5>
2025-03-28 19:56:36 +0400

    Create In utils

--- PS1/src/utils.ts
+++ PS1/src/utils.ts
@@ -1,7 +1,7 @@
 // This file is for utility functions that might be helpful
 // for both implementation and testing code.
 // Place any reusable helper functions here.
-
+import { Flashcard } from "./flashcards";
 // Example utility function (you can remove this):
 /**
  * Adds two numbers together.
@@ -11,4 +11,7 @@
  */
 export function add(a: number, b: number): number {
   return a + b;
+}
+export function createGeneralCard(name: string): Flashcard{
+  return new Flashcard(name, "general", "general", [])
 }
